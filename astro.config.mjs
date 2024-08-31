7322703c80babb9098d6caed0194e9a97a8e8f38
Kival Evan <5645391+KivalEvan@users.noreply.github.com>
2024-08-31 11:21:26 +0800

    Remove polyfill

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,14 +5,5 @@ import solidJs from '@astrojs/solid-js';
 export default defineConfig({
    site: 'https://kivalevan.me',
    base: '/BeatSaber-MappingUtility',
-   integrations: [solidJs()],
-   vite: {
-      resolve: {
-         alias: {
-            'node:path': 'src/empty-polyfills.js',
-            'node:fs/promises': 'src/empty-polyfills.js',
-            'node:fs': 'src/empty-polyfills.js',
-         },
-      },
-   },
+   integrations: [solidJs()]
 });
--- src/empty-polyfills.js
+++
@@ -1,6 +0,0 @@
-export const resolve = null;
-export const basename = null;
-export const readFile = null;
-export const readFileSync = null;
-export const writeFile = null;
-export const writeFileSync = null;
